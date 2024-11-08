5f0681f6a31eb6c15239aa323d570d81aebc49cd
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2024-11-08 13:52:49 -0300

    Atualização
    adição de funcionalidades de serviços

--- routes/servicos.js
+++ routes/servicos.js
@@ -4,8 +4,33 @@ const mongoose = require("mongoose");
 
 const {eAdmin} = require("../helpers/eAdmin")
 
+const ServicesModel = require("../models/services")
+
 router.get("/servicos", eAdmin, (req, res) => {
-    res.render("admin/servicos");
+    ServicesModel.find({userId: req.user.id}).lean().then((service)=>{
+        console.log(service)
+        res.render("admin/servicos/servicos", {service: service})
+    }).catch((err)=>{
+        console.log("houve um erro: "+ err)
+        res.redirect("/servicos", {err})
+    })
+    res.render("admin/servicos/servicos");
 });
+router.post("/addservice", eAdmin,(req, res)=>{
+    const { name, description, value } = req.body;
+    const novoService = new ServicesModel({
+        name,
+        description,
+        value,
+        userId: req.user.id
+    });
+    novoService.save().then(()=>{
+        console.log("serviço adicionado")
+        res.redirect("/servicos")
+    }).catch((err)=>{
+        console.log(err)
+        res.redirect("/servicos")
+    })
+})
 
 module.exports = router
